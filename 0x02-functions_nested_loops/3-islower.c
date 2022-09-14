81214ff51f1c9c0400164542b1b8462b67d3bf57
callmrbabs <callmrbabs.com>
2022-09-13 18:21:35 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int _islower - To checks for lowercase character.
+ * @c : character to check the case
+ * Return : 0 or 1
+ */
+
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
