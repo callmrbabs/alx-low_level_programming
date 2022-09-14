f83e32d57f3a8be949cc357f6318c45f6412b6eb
callmrbabs <callmrbabs.com>
2022-09-13 17:14:30 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,16 @@
 
 
 /**
- * main - check the code
+ * print_alphabet - Entry point
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(ch);
+_putchar(c);
 }
 _putchar('\n');
-return (0);
 }
