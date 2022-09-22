8b62f843bb70c39e0a6e4a6d36b13bf9b79c8f37
callmrbabs <callmrbabs.com>
2022-09-22 11:56:10 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second sring
+ *
+ * Return: 0 if equal or c if is not equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int c = 0;
+
+	while (s1[a] != '\0' && c == 0)
+	{
+		c = s1[a] - s2[a];
+		a++;
+	}
+	return (b);
+}
