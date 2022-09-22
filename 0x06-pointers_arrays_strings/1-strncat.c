8396aa2fadc560ac6ab82a3c2ec91e4d68636d17
callmrbabs <callmrbabs.com>
2022-09-21 21:28:11 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: input parameter strings
+ * @src: input parameter strings
+ * @n: input parameter strings
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != 0)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
+}
+
