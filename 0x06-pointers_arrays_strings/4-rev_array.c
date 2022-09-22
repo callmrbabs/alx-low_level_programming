ac2f0948aed9aacdf7b379e7377584e88167519e
callmrbabs <callmrbabs.com>
2022-09-22 12:04:51 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: input parameter
+ * @n: input parameter
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp;
+
+	i = 0;
+	n = n - 1;
+	while (i < n)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		i++;
+		n--;
+	}
+}
