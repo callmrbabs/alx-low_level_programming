33f38caa3247ff638b65194df3c117b9084f0e36
callmrbabs <callmrbabs.com>
2022-09-28 07:25:55 -0700

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-100-set_string.c
-101-crackme_password
 #endif
