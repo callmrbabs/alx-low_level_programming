bfd2dcba40784b7e2927a90119a3c24a41ccf084
callmrbabs <callmrbabs.com>
2022-09-30 09:08:35 -0700

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name.
+ * @argc: is an argument counter for char.
+ * @argv: is an argument value for char.
+ * Return: Return always success.
+**/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	printf('%s\n" argv[i]);
+
+	return (0);
+}
