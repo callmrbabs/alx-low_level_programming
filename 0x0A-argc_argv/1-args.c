d3b688fb7844367e7e64a8a8716e12629aab2e44
callmrbabs <callmrbabs.com>
2022-09-30 09:25:09 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the number.
+ * @argc: is an argument counter for char
+ * @argv: is an argument value for char.
+ * Return: Return always success.
+**/
+
+int main(int argc, char argv[])
+{
+	printf("argc = %d\n", argc);
+
+	return (0);
+}
